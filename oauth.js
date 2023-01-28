c2807decbf8d426c9432967e4af9f2007edeea99
Ben Wang <firetruckbw@gmail.com>
2023-01-28 14:14:44 -0600

    add basic things

---
+++ oauth.js
@@ -0,0 +1,8 @@
+// get the url 
+var url = window.location.href;
+//getting the access token from url 
+var access_token = url.split("#")[1].split("=")[1].split("&")[0]; 
+// get the userid 
+var userId = url.split("#")[1].split("=")[2].split("&")[0]; 
+console.log(access_token); 
+console.log(userId);
